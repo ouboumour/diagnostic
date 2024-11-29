1c0337dfa8aae8e3ac4abc335cdaf68b9a693757
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-11-29 15:14:27 +0100

    :recycle: TDD > InterpreterServiceImplTest > Cycle 4 > refacto

--- src/main/java/fr/softwaymedical/diagnostic/interpreter/InterpreterServiceImpl.java
+++ src/main/java/fr/softwaymedical/diagnostic/interpreter/InterpreterServiceImpl.java
@@ -1,16 +1,18 @@
 package fr.softwaymedical.diagnostic.interpreter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterpreterServiceImpl implements InterpreterService {
 
     @Override
     public String getMedicalUnit(int healthIndex) {
-        if (healthIndex % 3 == 0 && healthIndex % 5 == 0)
-            return "Cardiologie, Traumatologie";
-        if (healthIndex % 3 == 0)
-            return "Cardiologie";
-        if (healthIndex % 5 == 0)
-            return "Traumatologie";
-        return  "";
+
+        List<String> medicalUnits = new ArrayList<>();
+        if (healthIndex % 3 == 0) medicalUnits.add("Cardiologie");
+        if (healthIndex % 5 == 0) medicalUnits.add("Traumatologie");
+        return String.join(", ", medicalUnits);
+
     }
 
 }
--- src/test/java/fr/softwaymedical/diagnostic/interpreter/InterpreterServiceImplTest.java
+++ src/test/java/fr/softwaymedical/diagnostic/interpreter/InterpreterServiceImplTest.java
@@ -1,7 +1,9 @@
 package fr.softwaymedical.diagnostic.interpreter;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class InterpreterServiceImplTest {
 
@@ -13,22 +15,22 @@ public class InterpreterServiceImplTest {
 
     @Test
     public void givenAMultipleOfThreeButNotFiveHI_whenGettingMedialUnit_thenReturnCardiology() {
-        Assertions.assertEquals("Cardiologie", interpreterService.getMedicalUnit(33));
+        assertEquals("Cardiologie", interpreterService.getMedicalUnit(33));
     }
 
     @Test
     public void givenAMultipleOfFiveButNotThreeHI_whenGettingMedialUnit_thenReturnTraumatology() {
-        Assertions.assertEquals("Traumatologie", interpreterService.getMedicalUnit(55));
+        assertEquals("Traumatologie", interpreterService.getMedicalUnit(55));
     }
 
     @Test
     public void givenAMultipleOfBothFiveAndThreeHI_whenGettingMedialUnit_thenReturnBothCardiologyAndTraumatology() {
-        Assertions.assertEquals("Cardiologie, Traumatologie", interpreterService.getMedicalUnit(15));
+        assertEquals("Cardiologie, Traumatologie", interpreterService.getMedicalUnit(15));
     }
 
     @Test
     public void givenANeitherMultipleOfFiveNorThreeHI_whenGettingMedialUnit_thenReturnEmptyString() {
-        Assertions.assertTrue(interpreterService.getMedicalUnit(4).isEmpty());
+        assertTrue(interpreterService.getMedicalUnit(4).isEmpty());
     }
 
 }
