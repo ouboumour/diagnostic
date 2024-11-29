49b64efe17aa6f752907d99ae7015aab626c1dd4
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-11-29 14:59:29 +0100

    :white_check_mark: TDD > InterpreterServiceImplTest > Cycle 1 > add min code so that the test pass

---
+++ src/main/java/fr/softwaymedical/diagnostic/interpreter/InterpreterService.java
@@ -0,0 +1,7 @@
+package fr.softwaymedical.diagnostic.interpreter;
+
+public interface InterpreterService {
+
+    String getMedicalUnit(int healthIndex);
+
+}
---
+++ src/main/java/fr/softwaymedical/diagnostic/interpreter/InterpreterServiceImpl.java
@@ -0,0 +1,10 @@
+package fr.softwaymedical.diagnostic.interpreter;
+
+public class InterpreterServiceImpl implements InterpreterService {
+
+    @Override
+    public String getMedicalUnit(int healthIndex) {
+        return "Cardiologie";
+    }
+
+}
